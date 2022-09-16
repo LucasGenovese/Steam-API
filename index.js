cd5ac7c9bbef87589755e49d840f836ad908c9b4
Lucas Genovese Desktop <lucas59311@gmail.com>
2022-09-16 02:45:27 -0300

    Program tells you if the game has trading cards and their prices

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const cheerio = require('cheerio');
+
 const app = require('express')();
 const PORT = 3000;
 
@@ -9,26 +10,41 @@ let priceList = [];
 request('https://store.steampowered.com/search/?sort_by=Price_ASC&maxprice=350&category1=998&os=win&filter=topsellers', (error, response, html)=> { // gets price and game name
     if (!error && response.statusCode == 200){
         const $ = cheerio.load(html);
-        
-        $('.title').each(function(i,element) {
-            let gameTitle = $(this).text();
-            gameList.push(gameTitle);
-            //console.log(gameTitle);
+
+        // gets game titles
+        $('.col.search_capsule').each(function(i,element) { 
+            let gameId = $(this).find('img').attr('src').split("/")[5].trim(); // grabs game ID from img src and then trims the ID
+
+            // filters if games has trading cards
+            request('https://steamcommunity.com/market/search?q=&category_753_Game%5B%5D=tag_app_' + gameId + '&category_753_cardborder%5B%5D=tag_cardborder_0&category_753_item_class%5B%5D=tag_item_class_2&appid=753', (error, response, html)=> {
+                if (!error && response.statusCode == 200){
+                    const $ = cheerio.load(html);
+                    let tradingCardPrice = $('.normal_price').text();
+                    let gameName = $('.market_searchedForTerm').text();
+
+                    if (tradingCardPrice){
+                        console.log("precio del cromo para " + gameName + " :" + tradingCardPrice);
+                    } else {
+                        console.log( gameName + " does not have trading cards");
+                    }
+                }
+            })
+
+            gameList.push(gameId); // with this line appends EVERY game
         });
-        
+
+        // gets game price
         $('.col.search_price.discounted.responsive_secondrow').each(function(i,element){
             let gamePrice = $(this).first().text(); // retrieves price
             let cutPrice = gamePrice.split("$")[2].trim(); // grabs correct price by splitting string
             let flPrice = parseFloat(cutPrice.replace(/,/g, '.')); // price parsed into float
             priceList.push(flPrice);
-            //console.log(flPrice);
         })
 
     } else {
         console.log("Wrong response code: " + response.statusCode);
     }
 });
-
 
 /*
 app.listen(
@@ -38,8 +54,8 @@ app.listen(
 
 app.get('/game-list', (req, res) => {
     res.status(200).send({
-        prueba: 'algo',
-        prueba2: 'otro algo'
+        gameList: gameList,
+        priceList: priceList
     })
 });
 */
