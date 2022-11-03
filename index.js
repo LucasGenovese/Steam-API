a5ad2de5d6fb5a909aa0ccab3292a15bae11b0dc
Lucas Genovese Desktop <lucas59311@gmail.com>
2022-11-02 22:15:40 -0300

    Shows longer game list and switched to node-fetch instead of request

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const request = require('request');
-const cheerio = require('cheerio');
+import fetch from "node-fetch";
+import * as cheerio from "cheerio";
+import express from "express";
 
-const app = require('express')();
 const PORT = 3000;
+const app = express();
 
-const fee = 0.87;
-
-// replace with your own cookies
-const webTradeEligibility = 'XXXXXX';
+// replace with own steam marketplace cookies
+const webTradeEligibility = 'XXXXXXX';
 const browserid = 'XXXXXXX';
-const steamLoginSecure = 'XXXXXX';
+const steamLoginSecure = 'XXXXXXX';
 const sessionid = 'XXXXXXX';
 
-let fullList = []; 
+let idPriceList = [];
+let fullList = [];
 
-let tradingCardPricefl;
+class idPrice{
+    constructor(price, gameId){
+        this.price = price;
+        this.gameId = gameId;
+    }
+}
 
 class fullDesc{
     constructor(name, gamePrice, cardAmmount, profit){
@@ -25,63 +30,72 @@ class fullDesc{
     }
 }
 
-function getInfo(){
-    request('https://store.steampowered.com/search/?sort_by=Price_ASC&maxprice=840&category1=998&category2=29', (error, response, html)=> { // gets price and game name
-        if (!error && response.statusCode == 200){
-            const $ = cheerio.load(html);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-            // gets game titles
-            $('.col.search_capsule').each(function(i,element) { 
-                let gameId = $(this).find('img').attr('src').split("/")[5].trim(); // grabs game ID from img src and then trims the ID
+async function filterTradingCard(price, gameId){
+    const fee = 0.87;
 
-                let price = $(this).nextUntil('.col.search_price.discounted.responsive_secondrow').text().trim(); // gets game price
-                price = price.split('$')[2];
-                price = parseFloat(price.replace(',','.'));
-                
-                var options = {
-                    'method': 'GET',
-                    'url': 'https://steamcommunity.com/market/search?q=&category_753_Game%5B%5D=tag_app_' + gameId + '&category_753_cardborder%5B%5D=tag_cardborder_0&category_753_item_class%5B%5D=tag_item_class_2&appid=753#p1_price_asc',
-                    'headers': {
-                        'Cookie': 'webTradeEligibility='+ webTradeEligibility +' ; browserid='+ browserid +' ; steamLoginSecure= ' + steamLoginSecure +' ; sessionid=' + sessionid
-                    }
-                };
-
-                // filters if games has trading cards
-                request(options, (error, response, html)=> {
-                    if (!error && response.statusCode == 200){
-                        const $ = cheerio.load(html);
-                        let tradingCardPrice = $('.normal_price').text().trim();
-                        let tradingCardPriceTrimmed = tradingCardPrice.trim().split(' ')[3]; //trims the trading card price
-
-                        let tradingCardAmmount = $('#searchResults_total').text() //gets ammount of trading cards
-                        tradingCardAmmount = parseFloat(tradingCardAmmount);
-
-                        if (tradingCardPriceTrimmed){ // validates that the value is not undefined
-                            tradingCardPricefl = parseFloat(tradingCardPriceTrimmed.replace(',','.'));
-                        }
-                        
-                        let gameName = $('.market_listing_game_name').first().text().trim();
-                        let gameNameTrimmed = gameName.split(' ').slice(0, -2).join(' '); // cleans the game name (removes last two words)
-
-                        let possibleCards = Math.round(tradingCardAmmount/2);
-                        let calculateProfit = (possibleCards * (tradingCardPricefl)*fee) - price; // calculates profit
-
-                        if (tradingCardPrice && (calculateProfit>0)){ // validates undefined and checks if profitable
-                            console.log(gameNameTrimmed + " ✅");
-                            console.log("Game price: " + price);
-                            console.log("Possible trading cards dropped: " + possibleCards);
-                            console.log("Possible profit: $" + calculateProfit);
-
-                            let makeNode = new fullDesc(gameNameTrimmed, price, possibleCards, calculateProfit); // sends the data to an structure
-                            fullList.push(makeNode);
-                        }
-                    }
-                })
-            });
-
-        } else {
-            console.log("Wrong response code: " + response.statusCode);
+    const response = await fetch('https://steamcommunity.com/market/search/render/?query=&start=0&count=10&search_descriptions=0&sort_column=price&sort_dir=asc&appid=753&category_753_Game%5B%5D=tag_app_'+gameId+'&category_753_cardborder%5B%5D=tag_cardborder_0&category_753_item_class%5B%5D=tag_item_class_2', {
+        mode: 'cors',
+        headers: {
+            'Cookie': 'webTradeEligibility='+ webTradeEligibility +' ; browserid='+ browserid +' ; steamLoginSecure= ' + steamLoginSecure +' ; sessionid=' + sessionid
         }
+    });
+    const data = await response.json();
+
+    // gets price, trims it and parses it to float
+    const $ = cheerio.load(data.results_html);
+    let tradingCardPrice = $('.normal_price').text().trim();
+    let tradingCardPriceTrimmed = tradingCardPrice.trim().split(' ')[3];
+    
+    // validates that tradingCardPriceTrimmed is not undefined
+    if (tradingCardPriceTrimmed){
+        tradingCardPriceTrimmed = parseFloat(tradingCardPriceTrimmed.replace(',','.'));    
+    }
+
+    // gets ammount of cards and parses it to float
+    let tradingCardAmmount = parseFloat(data.total_count);
+
+    // gets and trimms game name
+    let gameName = $('.market_listing_game_name').first().text().trim();
+    let gameNameTrimmed = gameName.split(' ').slice(0, -2).join(' ');
+
+    // calculates profit
+    let possibleCards = Math.round(tradingCardAmmount/2);
+    let calculateProfit = (possibleCards * (tradingCardPriceTrimmed)*fee) - price;
+
+    // shows only profitable games
+    if (calculateProfit > 0){
+        console.log(gameNameTrimmed + " ✅");
+        console.log("Game price: " + price);
+        console.log("Possible trading cards dropped: " + possibleCards);
+        console.log("Possible profit: $" + calculateProfit);
+
+        let makeNode = new fullDesc(gameNameTrimmed, price, possibleCards, calculateProfit); // sends the data to an structure
+        fullList.push(makeNode);
+    }
+}
+
+
+async function getInfo(){
+
+    const response = await fetch ('https://store.steampowered.com/search/results/?query&start=0&count=100&dynamic_data=&sort_by=Price_ASC&snr=1_7_7_230_7&maxprice=840&category1=998&category2=29&infinite=1',
+    {mode: 'cors'});
+    const data = await response.json();
+
+    const $ = cheerio.load(data.results_html);
+
+    $('.col.search_capsule').each(function(i,element) { 
+        let gameId = $(this).find('img').attr('src').split("/")[5].trim(); // grabs game ID from img src and then trims the ID
+        
+        let price = $(this).nextUntil('.col.search_price.discounted.responsive_secondrow').text().trim(); // gets game price
+        price = price.split('$')[2];
+        price = parseFloat(price.replace(',','.'));
+        
+        let idPriceNode = new idPrice(price, gameId);
+        idPriceList.push(idPriceNode);
     });
 }
 
@@ -90,14 +104,31 @@ app.listen(
     () => console.log(`it's alive on http:localhost:${PORT}`)
 )
 
-// once getInfo is ready it will send the list in the GET method
-async function getList(){
-    getInfo();
+async function main(){
+    // generates idPriceList 
+    await getInfo();
+
+    // makes two different arrays for each data type so it can pass them to filterTradingCard function
+    let priceList =  idPriceList.map(({price}) => price);
+    let idList = idPriceList.map(({gameId}) => gameId); 
+
+    for (let i=0; i<idPriceList.length; i++){
+
+        // every 20 requests waits 5 seconds so it wont block me for attempting too much
+        if (i%20 === 0 && i!=0){
+            await sleep(5000);
+        }
+        
+        // retrieves and makes list of profitable games
+        await filterTradingCard(priceList[i], idList[i]);
+    }
+
+    // finally it pushes the list made by filterTradingCard
     app.get('/game-list', (req, res) => {
         res.status(200).send({
             fullList: fullList
         })
-    });    
+    }); 
 }
 
-getList();
+main();
